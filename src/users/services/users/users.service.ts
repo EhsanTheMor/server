a1203b81260bac448949602762c8b845f4945b02
Ehsan Morshedzadeh <ehsan.morshedzadeh@gmail.com>
2023-11-08 20:29:09 +0330

    Inserted repository to service,

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from "../../entities/users.entity";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {
+  }
+
+  saveUser(user: any){
+    console.log(user);
+  }
+}
