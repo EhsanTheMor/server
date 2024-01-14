53c0f97b40aa3cb437de326d77dfa4f092860928
Ehsan Morshedzadeh <ehsan.morshedzadeh@gmail.com>
2024-01-14 23:07:22 +0330

    serialized out going data from auth

--- src/user/controller/auth/auth.controller.ts
+++ src/user/controller/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from 'src/user/dtos/create-user.dto';
 import { SignInDto } from 'src/user/dtos/sign-in.dto';
 import { SignUpDto } from 'src/user/dtos/sign-up.dto';
+import { UserDto } from 'src/user/dtos/user.dto';
 import { AuthService } from 'src/user/service/auth/auth.service';
 
 @Controller('auth')
@@ -10,11 +12,13 @@ export class AuthController {
     private authService: AuthService
   ) { }
 
+  @Serialize(UserDto)
   @Post('signin')
   signIn(@Body() body: SignInDto) {
     return this.authService.signIn(body.email, body.password);
   }
 
+  @Serialize(UserDto)
   @Post('/signup')
   signUp(@Body() body: SignUpDto) {
     return this.authService.signUp(body);
