49a467d321ded4ddf4de1a7e5d0cbad51cf0ec9e
Ehsan Morshedzadeh <ehsan.morshedzadeh@gmail.com>
2024-01-20 09:33:28 +0330

    Solved jwt interceptor problem

--- src/user/controller/auth/auth.controller.ts
+++ src/user/controller/auth/auth.controller.ts
@@ -1,20 +1,15 @@
-import { Body, Controller, Post, Res, UseInterceptors } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { Response } from 'express';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SignInDto } from 'src/user/dtos/sign-in.dto';
 import { SignUpDto } from 'src/user/dtos/sign-up.dto';
-import { UserDto } from 'src/user/dtos/user.dto';
-import { JwtInterceptor } from 'src/user/interceptors/jwt.interceptor';
+import { UseJwt } from 'src/user/interceptors/jwt.interceptor';
 import { AuthService } from 'src/user/service/auth/auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private authService: AuthService,
-  ) { }
+  constructor(private authService: AuthService) {}
 
   @Post('signin')
+  @UseJwt()
   signIn(@Body() body: SignInDto) {
     return this.authService.signIn(body.email, body.password);
   }
--- src/user/interceptors/jwt.interceptor.ts
+++ src/user/interceptors/jwt.interceptor.ts
@@ -1,23 +1,45 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ServerResponse } from 'http';
 import { Observable, map } from 'rxjs';
+import { jwtConstants } from 'src/constants/constants';
+
+export function UseJwt() {
+  return UseInterceptors(new JwtInterceptor(new JwtService()));
+}
 
 export class JwtInterceptor implements NestInterceptor {
-  constructor(private jwtService: JwtService) { }
+  constructor(private jwtService: JwtService) {}
 
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
+    const incomingReq = context.switchToHttp().getRequest<Request>();
+    const authToken = this.extractTokenFromHeader(incomingReq);
+    try {
+      const result = this.jwtService.verify(authToken.toString(), {
+        secret: jwtConstants.secret,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+
     //Attach data to the headers.
     return next.handle().pipe(
       map((data: any) => {
         const request = context.switchToHttp().getResponse<ServerResponse>();
-        console.log(this.jwtService)
-        request.setHeader('Authorization', 'Bearer ');
         return data;
       }),
     );
+  }
+
+  extractTokenFromHeader(req: Request): string {
+    return req.headers['authorization']?.split(' ')[1];
   }
 }
