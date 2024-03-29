23f6ea578762ce763631eafe806a087a6c281303
EhsanTheMor <ehsan.morshedzade@gmail.com>
2024-03-29 11:40:46 +0330

    Completed user controller

--- src/user/controller/user/user.controller.ts
+++ src/user/controller/user/user.controller.ts
@@ -1,12 +1,22 @@
-import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUserGuard } from 'src/post/guards/current-user.guard';
 import { CreateUserDto } from 'src/user/dtos/create-user.dto';
+import { UserDto } from 'src/user/dtos/user.dto';
 import { UserService } from 'src/user/service/user/user.service';
 
 @UseGuards(CurrentUserGuard)
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
   @Get()
   getAllUsers() {
